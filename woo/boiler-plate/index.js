dc28809d9c70bd10a49461ef73a845849adfc036
wooguk <ows8888@gmail.com>
2022-07-05 23:06:47 +0900

    no message

--- woo/boiler-plate/index.js
+++ woo/boiler-plate/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3300
 //연결 안되고 나네... 뭐지 ....
+//몽고 디비랑 몽구스는 무엇이지??
+//mongoose를 설치해야 됨
+//mongodb+srv://wooguk:<password>@cluster0.2atho59.mongodb.net/?retryWrites=true&w=majority
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://wooGuk:user01@boilerplate.fwjahgt.mongodb.net/?retryWrites=true&w=majority',{
-    useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
+mongoose.connect('mongodb+srv://wooguk:jjangws7@cluster0.2atho59.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser:true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected..'))
 .catch(err => console.log(err));
 
