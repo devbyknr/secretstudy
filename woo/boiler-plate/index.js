5a44e29e006519d22ff2596a0dcead7937d0803f
wooguk <ows8888@gmail.com>
2022-07-09 22:08:48 +0900

    login까지 했는데, 좀 이상하다...

--- woo/boiler-plate/index.js
+++ woo/boiler-plate/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3300
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const {User} = require("./models/user");
 //연결 안되고 나네... 뭐지 ....
 //몽고 디비랑 몽구스는 무엇이지??
@@ -13,6 +15,7 @@ const {User} = require("./models/user");
 app.use(bodyParser.urlencoded({extended:true}));
 //application/json형태를 분석해서 가져옴 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const config = require('./config/key');
 const mongoose = require('mongoose')
@@ -31,11 +34,42 @@ app.post('/register',(req, res)=>{
   //회원가입할때 필요한 정보들을 클라이언트에서 가져오면 그것들을 디비에 넣어준다.
   const user = new User(req.body);
   user.save((err, userInfo) =>{
-    if(err) return res.json({success : false, err})
+    if(err){console.log(err); return res.json({success : false, err})}
     return res.status(200).json({
-      success :true
+      success :true,
+      info : user
     })
   });
+})
+
+app.post('/login',(req, res) => {
+  //DB에서 요청된 정보 찾기
+  User.findOne({emai:req.body.email} , (err,userInfo) => {
+    if(!userInfo){
+      return res.json({
+        loginSuccess : false,
+        message : "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    userInfo.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch){
+        return res.json({loginSuccess : false, message : "비밀번호가 틀렷습니다"});
+      }
+      //비밀번호까지 맞다면 USER를 위한 토큰을 생성해야함.
+      userInfo.generateToken((err, userInfo) =>{
+          if(err){
+            return res.status(400).send(err);
+          }
+          //토큰을 저장한다. 어디에?? 쿠키, 로컬스트로지
+          res.cookie("x_auth",userInfo.token)
+          .status(200)
+          .json({loginSuccess : true, userId: userInfo._id}) 
+      })
+    })
+  })
+  
+  
 })
 
 app.listen(port, () => {
--- woo/boiler-plate/models/user.js
+++ woo/boiler-plate/models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
     name : {
@@ -34,6 +38,47 @@ const userSchema = mongoose.Schema({
         type : Number
     }
 })
+
+userSchema.pre('save', function(next){
+    let user = this;
+    if(user.isModified('password')){
+        //비밀번호를 암호화 시킨다
+        bcrypt.genSalt(saltRounds,function(err, salt){
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, function(err,hash){
+                    if(err) return next(err);
+                    
+                        user.password = hash
+                        next();
+                    
+                })
+        })
+    } else {
+        next();
+    }
+
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //plainPassword 1234567 암호화된 비밀번호
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+
+    let user = this;
+    //jsonwebtoken을 이용해서 token을생성하기
+    let token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null,user)
+    })
+
+}
 
 const User = mongoose.model('User',userSchema)
 
