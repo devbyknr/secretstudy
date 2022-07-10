8d329fa8dbf5080441ea27539671e28fb3a1307d
Nuri Kim <enrghim@gmail.com>
2022-07-10 21:38:05 +0900

    1w #11#12 로그인/토큰생성

--- nuri/boiler-plate/index.js
+++ nuri/boiler-plate/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 5000 //임의 포트 설정
 
+const config = require('./config/key');
 const {User} = require("./models/User");
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 //application/x-www-form-urlencoded
 //이렇게된 데이터를 분석해서 가져올 수 있게 해주는 것
@@ -15,7 +17,7 @@ app.use(bodyParser.json());
 
 //몽고DB연결(version 3.0 or later)
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://nuriKim:snflwkfl1!@cluster0.donwl.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser : true, useUnifiedTopology : true
   //, useCreateIndex : true, useFindAndModify : false
   //useCreateIndex, useFindAndModify 옵션 지원안하므로 주석처리
@@ -34,6 +36,7 @@ app.post('/register', (req, res) => {
     const user = new User(req.body)
     //req.body에 client에서 보내는 정보를 담음
 
+    //save 전, 비밀번호 암호화 로직 추가
     user.save((err, doc) => {//정보를 user 모델에 저장
       if(err) return res.json({
         success : false, err //suceess false, 에러메세지 json타입으로 리턴
@@ -42,9 +45,42 @@ app.post('/register', (req, res) => {
         success : true,
         message : "회원가입 완료"
       });
+    })  
+})
+
+//로그인 route 생성(endpoint는 login)
+app.post('/login', (req, res) => {
+  //1. DB에서 요청된 이메일 찾기
+  User.findOne({email : req.body.email}, (err, user) => {
+    if(!user){
+      return res.json({
+        loginSuccess : false,
+        message : "이메일에 해당하는 회원 없음"
+      })
+    }
+  
+    //2. 이메일의 비밀번호가 같은지 확인
+    user.comparePassword(req.body.password, (err, isMatch)=>{
+      //comparePassword User모델에 생성
+      if(!isMatch) 
+        return json({loginSuccess : false, message : '비밀번호 틀림'})
+      
+      //3. 비밀번호가 일치하면 토큰 생성
+      user.generateToken((err, user)=>{
+      if(err) {
+        res.status(400).send(err)
+        console.log(err)
+      }
+
+      //토큰 저장 -> 쿠키/세션/ 로컬스토리지 등
+      //일단 쿠키에 저장해보기 -> express Cookie parser 설치
+      console.log("user token : "+user.token)
+      res.cookie("x_auth", user.token) //cookie에 x_auth란 이름의 필드에 토큰 저장
+      .status(200)
+      .json({loginSuccess : true, userId : user._id})
+      })
     })
-    
-    
+  })
 })
 
 app.listen(port, () => {
--- nuri/boiler-plate/models/User.js
+++ nuri/boiler-plate/models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10 //10자리인 salt를 생성한 후, 이것을 사용해 암호화
+
+const jwt = require('jsonwebtoken')
 
 //스키마 선언
 const userSchema = mongoose.Schema({
@@ -13,7 +17,7 @@ const userSchema = mongoose.Schema({
     },
     password : {
         type : String,
-        maxlength : 5
+        maxlength : 100
     },
     lastname : {
         type : String,
@@ -32,6 +36,50 @@ const userSchema = mongoose.Schema({
         type : Number
     }
 })
+
+//usermodel의 정보를 저장(save function) 전, pre function 설정
+userSchema.pre('save', function(next){
+    var user = this; //userSchema 객체
+
+    if(user.isModified('password')){ //model > password 필드가 수정하는 경우
+        //bcrypt library로 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err) //err 나면 save 실행
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                //user.password = 사용자가 입력한 비밀번호 평문, hash = salt를 이용해 평문을 암호화한 값
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, callbackfunc){
+    //평문을 암호화해서 db 값과 비교
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return callbackfunc(err)
+        callbackfunc(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(callbackfunc){
+    //jsonwebtoken 이용해 토큰 생성
+    var user = this;
+
+    //user._id + secretToken이란 txt를 이용해 Token 생성
+    //secretToken을 이용해 user._id 도출 가능
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return callbackfunc(err)
+        callbackfunc(null, user)
+    })
+}
 
 //모델은 스키마를 감싸주는 역할 model(모델명, 스키마)
 const User = mongoose.model('User', userSchema)
