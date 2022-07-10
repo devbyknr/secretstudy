9d2af897f97a46295e1f6890c9d10cb6fa0780e6
hcPark <zipcodebaby@gmail.com>
2022-07-10 17:32:45 +0900

    key

--- hyun/index.js
+++ hyun/index.js
@@ -4,15 +4,34 @@ const express = require('express')
 const app = express()
 const port = 7040
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hcpark:xpBWZNIcIU6tT1de@boilerplate.bvlwj.mongodb.net/?retryWrites=true&w=majority').then(()=>console.log('MongoDB connected...')).catch(err => console.log(err))
+const { User }  = require('./models/User');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config/key');
+mongoose.connect(config.mongoURI).then(()=>console.log('MongoDB connected...')).catch(err => console.log(err))
 
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
 
-
+//application/json
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 아ㄴ녕ㅏ세')
+  res.send('Hello World! 아ㄴ녕ㅏ세asdfasdf')
+})
+
+app.post('/register',(req,res)=>{
+  //회원가입시 정보 가져오기
+
+  const user = new User(req.body);
+  user.save((err,userInfo)=>{
+    if(err) return res.json({success:false,err})
+    return res.status(200).json({success:true})
+  });
+  
+  //디비에 넣기
+
 })
 
 app.listen(port, () => {
