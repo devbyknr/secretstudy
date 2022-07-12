62d71bf2838529c946bb0d7d2fc663506eea0c41
wooguk <ows8888@gmail.com>
2022-07-12 22:46:01 +0900

    우석 react 4회차 까지

---
+++ woo/boiler-plate/client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Reacts</p>
+        <p>public 디렉터리 안에는 webpack이 관리를 해주지 않으니</p>
+        <p>src영역에다가 넣어야 한다.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
