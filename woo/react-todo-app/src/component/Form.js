8294f02eb608da919b3a80fc3be478f1197820a3
wooguk <ows8888@gmail.com>
2022-07-28 23:51:32 +0900

    TailWind CSS 적용하여 할일 목록 스타일링 재정의

---
+++ woo/react-todo-app/src/component/Form.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Form({ handleSubmit, setValue, value }) {
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <form
+        style={{ display: "flex" }}
+        onSubmit={handleSubmit}
+        className="flex pt-2"
+      >
+        <input
+          type="text"
+          name="value"
+          className="w-full px-3 py-2 mr-4 text-gray-500 border-rounded shadow"
+          placeholder="해야 할 일을 입력하세요"
+          value={value}
+          onChange={handleChange}
+        />
+        <input
+          type="submit"
+          value="입력"
+          className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-400"
+          style={{ flex: "1" }}
+        />
+      </form>
+    </div>
+  );
+}
